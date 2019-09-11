f8501e03fadb99398fae38d308f90888be349fe6
egutten <guttenberg.emily@gmail.com>
2019-09-11 09:17:34 -0700

    conversion data sent to customer and customer activities tables

--- conversion.js
+++ conversion.js
@@ -1,41 +1,56 @@
 function Conversion() {
 
-  this.init = function(config) {
-    $.ajax({
-      url: "http://localhost:8080/customer-update",
-      type: "PUT",
-      dataType: "json",
-      data: {
-        email: config.email,
-        first_name: config.first_name,
-        last_name: config.last_name,
-        company_name: config.company_name,
-        customer_id: Number(this.getCookieValue('customer_id'))
-      },
-      error: function(error) {
-        console.log(error);
-      },
-      success: function(response){
-        console.log(response);
-      }
-    });
+  this.updateCustomer = function(config) {
+    var params = {
+      email: config.email,
+      first_name: config.first_name,
+      last_name: config.last_name,
+      company_name: config.company_name,
+      customer_id: Number(this.getCookieValue('customer_id'))
+    }
+  
+    xhr = new XMLHttpRequest();
+    xhr.open('POST', 'http://localhost:8080/customer-update');
+    xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+    xhr.onload = function() {
+      if (xhr.readyState === 4) {  
+          if (xhr.status === 200) {  
+            console.log(xhr.responseText)  
+          } else {  
+             console.log("Error", xhr.statusText);  
+          }  
+      }  
+    };
+    xhr.send(JSON.stringify(params));
   }
 
-  // this.createConversion = function(user_data) {
-  // 
-  // }
+  this.updateActivity = function(config) {
+    var params = {
+      event: config.event,
+      conversion_event_id: config.conversion_event_id,
+      user_id: config.user_id,
+      customer_id: Number(this.getCookieValue('customer_id'))
+    }
+    
+    xhr = new XMLHttpRequest();
+    xhr.open('POST', 'http://localhost:8080/customer-activity-conversion');
+    xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+    xhr.onload = function() {
+      if (xhr.readyState === 4) {  
+          if (xhr.status === 200) {  
+            console.log(xhr.responseText)  
+          } else {  
+             console.log("Error", xhr.statusText);  
+          }  
+      }  
+    };
+    xhr.send(JSON.stringify(params));
+  }
 
   this.getCookieValue = function(cookie_name) {
     var b = document.cookie.match('(^|[^;]+)\\s*' + cookie_name + '\\s*=\\s*([^;]+)');
     return b ? b.pop() : '';
   }
-  
-  
-  //2. insert new customer activity into customer activities table:
-    // event, 
-    // conversion event id, 
-    // user id, 
-    // customer id
 
 }
 
