5bb2ca90d9c627cd61b3ed8a7669dcafcb51bfc8
egutten <guttenberg.emily@gmail.com>
2019-09-16 11:26:55 -0700

    use asynchronous fetch requests instead of xhr synchronous requests; decided to access conversion_event_id when generating the snippet

--- conversion.js
+++ conversion.js
@@ -1,84 +1,52 @@
 function Conversion() {
 
   this.updateCustomer = function(config) {
-    var params = {
+    postData('http://localhost:8080/customer-update', {
       email: config.email,
       first_name: config.first_name,
       last_name: config.last_name,
       company_name: config.company_name,
       customer_id: Number(this.getCookieValue('customer_id'))
+    })
+    .then(data => console.log(JSON.stringify(data)))
+    .catch(error => console.log(error));
+    
+    function postData(url = '', data = {}) {
+      return fetch(url, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json;charset=UTF-8'},
+        body: JSON.stringify(data)
+      })
+      .then(response => response.json());
     }
-    xhr = new XMLHttpRequest();
-    xhr.open('POST', 'http://localhost:8080/customer-update', false);
-    xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-    xhr.onload = function() {
-      if (xhr.readyState === 4) {  
-          if (xhr.status === 200) {  
-            console.log(xhr.responseText)  
-          } else {  
-             console.log("Error", xhr.statusText);  
-          }  
-      }  
-    };
-    xhr.send(JSON.stringify(params));
   }
 
   this.updateActivity = function(config) {
-    var params = {
+    postData('http://localhost:8080/customer-activity-conversion', {
       event: config.event,
-      conversion_event_id: this.getConversionId(45, function(data) {
-          return data;
-      }),
       user_id: config.user_id,
+      conversion_event_id: config.conversion_event_id,
       customer_id: Number(this.getCookieValue('customer_id'))
+    })
+    .then(data => console.log(JSON.stringify(data)))
+    .catch(error => console.log(error));
+    
+    function postData(url = '', data = {}) {
+      return fetch(url, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json;charset=UTF-8'},
+        body: JSON.stringify(data)
+      })
+      .then(response => response.json());
     }
-    xhr = new XMLHttpRequest();
-    xhr.open('POST', 'http://localhost:8080/customer-activity-conversion', false);
-    xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-    xhr.onload = function() {
-      if (xhr.readyState === 4) {  
-          if (xhr.status === 200) {  
-            console.log(xhr.responseText)  
-          } else {  
-             console.log("Error", xhr.statusText);  
-          }  
-      }  
-    };
-    xhr.send(JSON.stringify(params));
   }
 
   this.getCookieValue = function(cookie_name) {
     var b = document.cookie.match('(^|[^;]+)\\s*' + cookie_name + '\\s*=\\s*([^;]+)');
     return b ? b.pop() : '';
   }
-  
-  
-  this.getConversionId = function(id, callback) {
-    var params = {
-      user_id: id
-    }
-    xhr = new XMLHttpRequest();
-    xhr.open('POST', 'http://localhost:8080/conversion-id', false);
-    xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-    xhr.onload = function() {
-      if (xhr.readyState === 4) {  
-          if (xhr.status === 200) { 
-            callback(JSON.parse(xhr.responseText));
-    
-          } else {  
-            console.log("Error", xhr.statusText);  
-          }  
-      }  
-    };
-    xhr.send(JSON.stringify(params));
-  }
-  
-  this.getConversionId(45, function(data) {
-      console.log(data);
-  });
+
 }
-
-
 
 var conversion = new Conversion();
 
